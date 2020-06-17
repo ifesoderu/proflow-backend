3b8d1686c221073c03cfeef37cad0f1b86a2199e
Emmanuel <akhigbemmanuel@gmai.com>
2020-06-17 21:30:50 +0100

    fixes

--- server.js
+++ server.js
@@ -104,7 +104,7 @@ app.get('/task/:id', (req, res) => { getTaskById(db, req, res) })
 
 
 //Get tasks by ids
-app.get('/task/:ids', (req, res) => { getTasksByIds(db, req, res) })
+app.get('/tasksbyids/:ids', (req, res) => { getTasksByIds(db, req, res) })
 
 //Get tasks by member id
 app.get('/personaltasks/:email', (req, res) => { getPersonalTasks(db, req, res) })
