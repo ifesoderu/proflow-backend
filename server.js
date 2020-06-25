0934225fd4c32e36598b67a4d07779609d3987b2
Emmanuel <akhigbemmanuel@gmai.com>
2020-06-25 16:17:16 +0100

    connect knex

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cors(
     {
-        origin: 'http://localhost:3000'
+        origin: 'https://proflowapp.herokuapp.com'
     }
 ));
 
