776f5f48f9f87806f5d69466630b0473494e829f
Emmanuel <akhigbemmanuel@gmail.com>
2020-10-28 10:08:26 +0100

    Change cors

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cors(
     {
-        origin: 'https://emekly.herokuapp.com'
+        origin: 'https://proflowapp.herokuapp.com'
     }
 ));
 
